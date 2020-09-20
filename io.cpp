d3734774ea56b66d59a6ebd529e406b2143b26a5
Naufal Taufiq Ridwan <71002775+maskods@users.noreply.github.com>
2020-09-21 00:42:34 +0700

    Create io.cpp

---
+++ io.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // Menampilkan
+  cout << "Hello World!" << endl << endl;
+  
+  // Membuat Variabel
+  string nama;
+  
+  // Input
+  cout << "Masukkan nama anda : ";
+  
+  // Memasukkan nilai yang  telah di input ke variabel nama
+  cin >> nama;
+  
+  // Menampilkan nilai dari variabel nama
+  cout << "Nama anda adalah " << nama;
+  
+return 0;
+}
